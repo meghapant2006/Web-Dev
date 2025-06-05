94a4240141c45d9bcbbf1716b8920c2cd0aa9fd2
Megha Pant <mp8774482@gmail.com>
2025-06-05 15:24:31 +0530

    okoko

---
+++ 3js/typeofjs/new.js
@@ -0,0 +1,7 @@
+console.log("Hello World")
+console.log("code is running ")
+console.log("code is also running")
+console.log("Code is look like a wow")
+
+
+
---
+++ 3js/typeofjs/script.js
@@ -0,0 +1,15 @@
+alert('hello')
+console.log("Hello, World!");
+
+var a = prompt("Hello my love")
+
+var i = confirm("do u love me")
+if(i){
+    console.log("yesssssss so much ")
+}
+else{
+    console.log("naa chiii")
+
+}
+
+console.log("i love u") 
